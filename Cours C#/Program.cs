c30ab5b9908c55c390d609faa2c7d8b62c768f98
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-07-09 09:14:16 +0200

    docs(Program.cs): initial commit ^& add functions doc

---
+++ Cours C#/Program.cs
@@ -0,0 +1,33 @@
+﻿// Functions (locales) en C#;
+
+// static void Fonction(){};
+//        <type de retour> <nom de la fonction><parametre><instructions>
+
+// pour appeler une fonction on utilise cette syntaxe;
+// Fonction(<arguments>){   };
+// le resulatat d'une fonction renvoi un : Le retour.
+
+void Bienvenue()
+{
+    Console.WriteLine("Hello World");
+};
+
+Bienvenue();
+
+// la signature d'une fonction : son nom, ses paramètres et son type de retour
+
+void BienvenueBis(String prenom = "Alex")
+{
+    Console.WriteLine($"Bonjour, {prenom} !");
+};
+
+BienvenueBis("Lucas");
+
+// une fonction peut renvoyer une valeur: elle effectue un "return"
+
+int Addition(int a, int b)
+{ 
+    return a + b;
+}
+
+Console.WriteLine(Addition(3, 5));
